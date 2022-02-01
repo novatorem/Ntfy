4841b1e529586c256a67cf29a6e89d6d48f262ac
Andrew Novac <16753077+novatorem@users.noreply.github.com>
2022-01-31 20:47:45 -0500

    Added a redirect bar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -50,6 +50,24 @@
 		<div class="flex-1 px-2 mx-2">
 			<span class="text-lg font-bold"> NTFY </span>
 		</div>
+
+		<a class="flex-none" href="https://ntfy.sh/{topic}" target="_blank">
+			<button class="btn btn-square btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block w-6 h-6 stroke-current text-success"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+					/>
+				</svg>
+			</button>
+		</a>
 	</div>
 	<div id="ntfy" class="h-2/3">
 		<input placeholder="Topic*" class="input input-bordered my-5 w-full" bind:value={topic} />
